bd8035467d36a52db452fa03bd6a289ba55132f3
Shakalov <yaroslav.shakalov@sigma.software>
2024-02-20 18:51:35 +0200

    add cw20 with restricted burn only for minter

---
+++ ixo-plus/contracts/cw20-base-lp/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cw20_base_lp::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
